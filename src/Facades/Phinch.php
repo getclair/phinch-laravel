3533f7cddfe855d6e8f21da79e16ee401b18bb22
Adam Campbell <adam@hotmeteor.com>
2022-09-07 09:47:25 -0500

    Update Phinch.php

--- src/Facades/Phinch.php
+++ src/Facades/Phinch.php
@@ -3,7 +3,24 @@
 namespace Phinch\Phinch\Facades;
 
 use Illuminate\Support\Facades\Facade as BaseFacade;
+use Phinch\PhinchClient;
 
+/**
+ * @method static authorize(string $token)
+ * @method static PhinchClient client()
+ * @method \Phinch\Products\Company company()
+ * @method \Phinch\Products\Directory directory()
+ * @method \Phinch\Products\Employment employment()
+ * @method \Phinch\Products\Individual individual()
+ * @method \Phinch\Products\Payment payment()
+ * @method \Phinch\Products\PayStatement payStatement()
+ * @method \Phinch\Products\Company $company
+ * @method \Phinch\Products\Directory $directory
+ * @method \Phinch\Products\Employment $employment
+ * @method \Phinch\Products\Individual $individual
+ * @method \Phinch\Products\Payment $payment
+ * @method \Phinch\Products\PayStatement $payStatement
+ */
 class Phinch extends BaseFacade
 {
     /**
