7cf9449a02540533219242d4768dbded37061105
Adam Campbell <adam@hotmeteor.com>
2022-09-07 09:53:06 -0500

    Update Phinch.php

--- src/Facades/Phinch.php
+++ src/Facades/Phinch.php
@@ -8,25 +8,16 @@ use Phinch\PhinchClient;
 /**
  * @method static authorize(string $token)
  * @method static PhinchClient client()
- * @method \Phinch\Products\Company company()
- * @method \Phinch\Products\Directory directory()
- * @method \Phinch\Products\Employment employment()
- * @method \Phinch\Products\Individual individual()
- * @method \Phinch\Products\Payment payment()
- * @method \Phinch\Products\PayStatement payStatement()
- * @method \Phinch\Products\Company $company
- * @method \Phinch\Products\Directory $directory
- * @method \Phinch\Products\Employment $employment
- * @method \Phinch\Products\Individual $individual
- * @method \Phinch\Products\Payment $payment
- * @method \Phinch\Products\PayStatement $payStatement
+ * @property \Phinch\Products\Company $company
+ * @property \Phinch\Products\Directory $directory
+ * @property \Phinch\Products\Employment $employment
+ * @property \Phinch\Products\Individual $individual
+ * @property \Phinch\Products\Payment $payment
+ * @property \Phinch\Products\PayStatement $payStatement
  */
 class Phinch extends BaseFacade
 {
     /**
-     * @method array providers()
-     * @method array introspect(string $token)
-     * @method array disconnect(string $token)
      *
      * @see \Phinch\Phinch
      */
